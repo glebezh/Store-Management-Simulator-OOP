75765b7bc3643a1d9422f025f03c731d912eb3c5
Bailey Atkinson <a1772413@ingb16w006.ad.adelaide.edu.au>
2023-10-06 14:51:55 +1030

    customer and employee

--- Customer.h
+++ Customer.h
@@ -10,6 +10,17 @@ class Customer: public People{
     protected:
         Item* items;
     public:
+    Customer(){
+        maxCarry = 0;
+        items = new Item[maxCarry];
+        name = NULL;
+        }
+    Customer(string name){
+        maxCarry = 3 + (rand() %7);
+        items = new Item[maxCarry];
+        this->name = name;
+    
+    }
         void checkout(Item* cart, Inventory shop, Store store){
             double cartPrice = 0
             for (int i = 0; i<this->getcarryAmount(); i++){
@@ -17,6 +28,7 @@ class Customer: public People{
             }
             shop.setMoney = shop.getMoney + cartPrice + 2*store.getRating();
         }
+        ~Customer(){delete items[]}
 };
 
 #endif
--- Employee.h
+++ Employee.h
@@ -4,12 +4,27 @@
 #include <string>
 #include "People.h"
 #include "Inventory.h"
+#include "Store.h"
 
 class Employee: public People{
     protected:
         double wageCost;
         double tipCost;
-    private:
+    public:
+        Employee(){
+            name = NULL
+            wageCost = 0;
+            tipcost = 0;
+            maxCarry = NULL
+            carryAmount = NULL
+        }
+        Employee(string name, Store shop){
+            this->name = name;
+            wageCost = 5 + (rand() %15);
+            tipCost =  shop.getRating();
+            maxCarry = NULL;
+            carryAmount = NULL;
+            }
         void drawWage(Inventory shop){
             shop.setMoney(shop.getMoney() - wageCost);
         }
