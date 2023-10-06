839230ba3b89f658ef0b790fc10382967ed548b8
Bailey Atkinson <a1772413@ingg22w069.ad.adelaide.edu.au>
2023-10-06 12:24:03 +1030

    people

---
+++ Employee.h
@@ -0,0 +1,17 @@
+#ifndef Employee_
+#define Employee_
+
+#include <string>
+#include "People.h"
+#include "Inventory"
+
+class Employee: public People{
+    protected:
+        double wageCost;
+        double tipCost:
+    private:
+        void drawWage(Inventory shop){
+            shop.money = shop.money - wageCost;
+        }
+
+};
